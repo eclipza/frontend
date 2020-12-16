2dc60847e04046d4126d319bd77abad6cafc662e
eclipza <40574350+eclipza@users.noreply.github.com>
2020-12-16 23:24:48 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+var deButton = document.querySelector("header button:first-of-type");
+
+deButton.addEventListener("click", openMenu);
+
+function openMenu() {
+
+    document.body.classList.toggle("menuOpen");
+    
+}
